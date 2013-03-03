4064ff12633412ef087a1455df9649c51aefe954
Daryl McMillan <daryl-mcmillan@example.com>
2013-03-03 09:49:33 -0500

    refactoring

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 function run( canvas ) {
 	var width = canvas.width;
 	var height = canvas.height;
+	var r = 25;
 	var ctx = canvas.getContext( "2d" );
-	console.log( ctx );
 	
 	var points = [];
 	for( var i=0; i<40;i++ ) {
@@ -15,20 +15,18 @@ function run( canvas ) {
 		points.push( point );
 	}
 	
-	var processFrame = function() {
-		animate( points, width, height );
-		draw( ctx, points, width, height );
-		setTimeout( processFrame, 10 );
+	var draw = function( ctx, points ) {
+		ctx.clearRect(0,0,width,height);
+		for( var i=0; i<points.length; i++ ) {
+			var point = points[i];
+			ctx.beginPath();
+			ctx.arc( point.x - r/2, point.y-r/2, r, r, 0, Math.PI*2, true );
+			ctx.closePath();
+			ctx.stroke();
+		}
 	};
-	processFrame();
 	
-}
-
-function animate( points, width, height ) {
-	for( var i=0; i<points.length; i++ ) {
-		var point = points[i];
-		point.x += point.vx;
-		point.y += point.vy;
+	var bounds = function( point ) {
 		if( point.x >= width && point.vx > 0
 			|| point.x <= 0 && point.vx < 0 ) {
 			point.vx = -point.vx;
@@ -37,17 +35,22 @@ function animate( points, width, height ) {
 			|| point.y <= 0 && point.vy < 0 ) {
 			point.vy = -point.vy;
 		}
-	}
-}
+	};
 
-function draw( ctx, points, width, height ) {
-	ctx.clearRect(0,0,width,height);
-	for( var i=0; i<points.length; i++ ) {
-		var point = points[i];
-		ctx.beginPath();
-		var r = 25;
-		ctx.arc( point.x - r/2, point.y-r/2, r, r, 0, Math.PI*2, true );
-		ctx.closePath();
-		ctx.stroke();
+	var animate = function( points ) {
+		for( var i=0; i<points.length; i++ ) {
+			var point = points[i];
+			point.x += point.vx;
+			point.y += point.vy;
+			bounds( point );
+		}
 	}
+	
+	var processFrame = function() {
+		animate( points );
+		draw( ctx, points, width, height );
+		setTimeout( processFrame, 10 );
+	};
+	processFrame();
+	
 }
